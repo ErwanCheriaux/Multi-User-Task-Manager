868347ff5a5346b0a5db3929709fece2996cfa1c
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2024-06-03 14:34:15 -0700

    add register and login form into modal

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Modal from '$lib/components/Modal.svelte';
+	import Register from '$lib/components/Register.svelte';
+	import Login from '$lib/components/Login.svelte';
+
+	let showRegisterModal = false;
+	let showLoginModal = false;
+</script>
+
+<h1>Welcome to Multi-User Task Manager</h1>
+<button on:click={() => (showRegisterModal = true)}>Register</button>
+<button on:click={() => (showLoginModal = true)}>Login</button>
+
+<Modal bind:showModal={showRegisterModal}>
+	<Register></Register>
+</Modal>
+
+<Modal bind:showModal={showLoginModal}>
+	<Login></Login>
+</Modal>
