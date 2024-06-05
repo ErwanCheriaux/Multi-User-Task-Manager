367598bf97bbd9683a51f959eef91efd4d0e204e
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2024-06-04 19:05:41 -0700

    insert common categories into db

---
+++ scripts/InsertCommonCategories.sql
@@ -0,0 +1,10 @@
+-- Insert rows into table '[dbo].[Categories]'
+INSERT INTO [dbo].[Categories]
+    ([Name])
+VALUES
+    ('Personal Development'),
+    ('Work'),
+    ('Home Maintenance'),
+    ('Health & Fitness'),
+    ('Social & Family');
+GO
